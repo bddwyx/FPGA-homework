171d7d72eb102e35444db00c28788aa44b9c85b8
bddwyx <56229174+bddwyx@users.noreply.github.com>
2020-11-04 09:54:21 +0800

    withFSM

--- Lock.srcs/sources_1/new/Lock.v
+++ Lock.srcs/sources_1/new/Lock.v
@@ -18,66 +18,39 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
-
-module D0_display( //注释参考实验一
- input [3:0] D0_bits ,
- input [3:0] D0_NUM ,
- output reg [6:0] D0_a_to_g ,
- output [3:0] D0_led_bits
- );
- assign D0_led_bits = D0_bits ; 
- always @(*) 
- begin 
- case(D0_NUM)
- 0:D0_a_to_g=7'b1111110;
- 1:D0_a_to_g=7'b0110000;
- 2:D0_a_to_g=7'b1101101;
- 3:D0_a_to_g=7'b1111001;
- 4:D0_a_to_g=7'b0110011;
- 5:D0_a_to_g=7'b1011011;
- 6:D0_a_to_g=7'b1011111;
- 7:D0_a_to_g=7'b1110000;
- 8:D0_a_to_g=7'b1111111;
- 9:D0_a_to_g=7'b1111011;
- 'hA: D0_a_to_g=7'b1110111;
- 'hB: D0_a_to_g=7'b0011111;
- 'hC: D0_a_to_g=7'b1001110;
- 'hD: D0_a_to_g=7'b0111101;
- 'hE: D0_a_to_g=7'b1001111;
- 'hF: D0_a_to_g=7'b1000111;
- default: D0_a_to_g=7'b1111110;
- endcase
- end
-endmodule
-
-
-module Count_FFFF( //注释参考实验二
- input clk,
- input clr,
- output [6:0] a_to_g ,
- output [3:0] led_bits
- );
- reg [3:0] num ;
- reg [35:0] ckl_cnt ;
- reg [3:0] t_led_bits ; //中间变量，存储数码管复用切换位控信息
-
- always@(posedge clk or posedge clr)
- begin
- if(~clr)
- ckl_cnt <= 0 ;
- else
- ckl_cnt <= ckl_cnt + 1 ;
- end
- 
- always@(*) //取计数变量的高 16 位组成要显示的 1 个 4 位十六进制数
- case(ckl_cnt[15:14]) //取计数变量的较低 2 位(频率高)数值，作为 4 位数码管扫描复位切换频率
- 0:begin num = ckl_cnt[23:20];t_led_bits = 4'b0001;end //数码管 1 有效，且显示数值为最低 4 位
- 1:begin num = ckl_cnt[27:24];t_led_bits = 4'b0010;end //数码管 2 有效，且显示数值为次低 4 位
- 2:begin num = ckl_cnt[31:28];t_led_bits = 4'b0100;end //数码管 3 有效，且显示数值为次高 4 位
- 3:begin num = ckl_cnt[35:32];t_led_bits = 4'b1000;end //数码管 4 有效，且显示数值为最高 4 位
- endcase
- 
- D0_display myD0_display(.D0_bits(t_led_bits),.D0_NUM(num),.D0_a_to_g(a_to_g),.D0_led_bits(led_bits)) ; 
- 
+module Lock(
+    input clk,
+    input clr,
+    input [3:0] pwdReg ,
+    input [3:0] pwdInput ,
+    output [6:0] a_to_g ,
+    output [3:0] led_bits
+    );
+    
+    reg displayFSM;
+    reg [15:0] infoBuf;
+    
+    always@(*) begin
+        if(displayFSM) begin
+                infoBuf[3:0] = pwdInput[0];
+                infoBuf[7:4] = pwdInput[1];
+                infoBuf[11:8] = pwdInput[2];
+                infoBuf[15:12] = pwdInput[3];
+            end
+        else begin
+                infoBuf[3:0] = pwdInput[0];
+                infoBuf[7:4] = pwdInput[1];
+                infoBuf[11:8] = pwdInput[2];
+                infoBuf[15:12] = pwdInput[3];
+            end    
+        //end
+    end
+    
+    DisplayTubes displayPwd( //Show PassWord
+             .clk(clk),
+             .clr(clr),
+             .dataBuf(infoBuf),
+             .a_to_g(a_to_g),
+             .led_bits(led_bits)
+     );
 endmodule
---
+++ Lock.srcs/sources_1/new/Top.v
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2020/11/04 08:27:38
+// Design Name: 
+// Module Name: Top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Top(
+    input clk,
+    input clr,
+    input [3:0] pwdReg,
+    input [3:0] pwdInput,
+    output [6:0] a_to_g ,
+    output [3:0] led_bits
+    );
+    Lock lock(
+        .clk(clk),
+        .clr(clr),
+        .pwdReg(pwdReg),
+        .pwdInput(pwdInput),
+        .a_to_g(a_to_g),
+        .led_bits(led_bits)
+    );
+endmodule
