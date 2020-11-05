4fa5d6f08ef4011dd9dccd96b7c13f67ed998a4b
bddwyx <56229174+bddwyx@users.noreply.github.com>
2020-11-05 12:09:22 +0800

    互斥锁

--- Lock.runs/impl_2/Top.tcl
+++ Lock.runs/impl_2/Top.tcl
@@ -61,96 +61,13 @@ proc step_failed { step } {
 }
 
 
-start_step init_design
-set ACTIVE_STEP init_design
-set rc [catch {
-  create_msg_db init_design.pb
-  set_param xicom.use_bs_reader 1
-  create_project -in_memory -part xc7a50tcsg324-1
-  set_property design_mode GateLvl [current_fileset]
-  set_param project.singleFileAddWarning.threshold 0
-  set_property webtalk.parent_dir E:/FPGA-homework/Lock.cache/wt [current_project]
-  set_property parent.project_path E:/FPGA-homework/Lock.xpr [current_project]
-  set_property ip_output_repo E:/FPGA-homework/Lock.cache/ip [current_project]
-  set_property ip_cache_permissions {read write} [current_project]
-  add_files -quiet E:/FPGA-homework/Lock.runs/synth_2/Top.dcp
-  read_xdc E:/FPGA-homework/Lock.srcs/constrs_1/new/Tube.xdc
-  link_design -top Top -part xc7a50tcsg324-1
-  close_msg_db -file init_design.pb
-} RESULT]
-if {$rc} {
-  step_failed init_design
-  return -code error $RESULT
-} else {
-  end_step init_design
-  unset ACTIVE_STEP 
-}
-
-start_step opt_design
-set ACTIVE_STEP opt_design
-set rc [catch {
-  create_msg_db opt_design.pb
-  opt_design 
-  write_checkpoint -force Top_opt.dcp
-  create_report "impl_2_opt_report_drc_0" "report_drc -file Top_drc_opted.rpt -pb Top_drc_opted.pb -rpx Top_drc_opted.rpx"
-  close_msg_db -file opt_design.pb
-} RESULT]
-if {$rc} {
-  step_failed opt_design
-  return -code error $RESULT
-} else {
-  end_step opt_design
-  unset ACTIVE_STEP 
-}
-
-start_step place_design
-set ACTIVE_STEP place_design
-set rc [catch {
-  create_msg_db place_design.pb
-  implement_debug_core 
-  place_design 
-  write_checkpoint -force Top_placed.dcp
-  create_report "impl_2_place_report_io_0" "report_io -file Top_io_placed.rpt"
-  create_report "impl_2_place_report_utilization_0" "report_utilization -file Top_utilization_placed.rpt -pb Top_utilization_placed.pb"
-  create_report "impl_2_place_report_control_sets_0" "report_control_sets -file Top_control_sets_placed.rpt"
-  close_msg_db -file place_design.pb
-} RESULT]
-if {$rc} {
-  step_failed place_design
-  return -code error $RESULT
-} else {
-  end_step place_design
-  unset ACTIVE_STEP 
-}
-
-start_step route_design
-set ACTIVE_STEP route_design
-set rc [catch {
-  create_msg_db route_design.pb
-  route_design 
-  write_checkpoint -force Top_routed.dcp
-  create_report "impl_2_route_report_drc_0" "report_drc -file Top_drc_routed.rpt -pb Top_drc_routed.pb -rpx Top_drc_routed.rpx"
-  create_report "impl_2_route_report_methodology_0" "report_methodology -file Top_methodology_drc_routed.rpt -pb Top_methodology_drc_routed.pb -rpx Top_methodology_drc_routed.rpx"
-  create_report "impl_2_route_report_power_0" "report_power -file Top_power_routed.rpt -pb Top_power_summary_routed.pb -rpx Top_power_routed.rpx"
-  create_report "impl_2_route_report_route_status_0" "report_route_status -file Top_route_status.rpt -pb Top_route_status.pb"
-  create_report "impl_2_route_report_timing_summary_0" "report_timing_summary -file Top_timing_summary_routed.rpt -warn_on_violation  -rpx Top_timing_summary_routed.rpx"
-  create_report "impl_2_route_report_incremental_reuse_0" "report_incremental_reuse -file Top_incremental_reuse_routed.rpt"
-  create_report "impl_2_route_report_clock_utilization_0" "report_clock_utilization -file Top_clock_utilization_routed.rpt"
-  close_msg_db -file route_design.pb
-} RESULT]
-if {$rc} {
-  write_checkpoint -force Top_routed_error.dcp
-  step_failed route_design
-  return -code error $RESULT
-} else {
-  end_step route_design
-  unset ACTIVE_STEP 
-}
-
 start_step write_bitstream
 set ACTIVE_STEP write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
+  set_param xicom.use_bs_reader 1
+  open_checkpoint Top_routed.dcp
+  set_property webtalk.parent_dir E:/FPGA-homework/Lock.cache/wt [current_project]
   catch { write_mem_info -force Top.mmi }
   write_bitstream -force Top.bit -raw_bitfile -bin_file
   catch {write_debug_probes -quiet -force Top}
--- Lock.srcs/sources_1/new/TubeDisplay.v
+++ Lock.srcs/sources_1/new/TubeDisplay.v
@@ -23,29 +23,33 @@ module D0_display( //One bit display
     input [3:0] D0_bits ,
     input [3:0] D0_NUM ,
     output reg [6:0] D0_a_to_g ,
-    output [3:0] D0_led_bits
+    output [3:0] D0_led_bits ,
+    input switchFlag
 );
     assign D0_led_bits = D0_bits ; 
+
     always @(*) begin 
-         case(D0_NUM)
-             0:D0_a_to_g=7'b1111110;
-             1:D0_a_to_g=7'b0110000;
-             2:D0_a_to_g=7'b1101101;
-             3:D0_a_to_g=7'b1111001;
-             4:D0_a_to_g=7'b0110011;
-             5:D0_a_to_g=7'b1011011;
-             6:D0_a_to_g=7'b1011111;
-             7:D0_a_to_g=7'b1110000;
-             8:D0_a_to_g=7'b1111111;
-             9:D0_a_to_g=7'b1111011;
-             'hA: D0_a_to_g=7'b0000000; //Turn off
-             'hB: D0_a_to_g=7'b0011111;
-             'hC: D0_a_to_g=7'b1001110;
-             'hD: D0_a_to_g=7'b1110110; //'N'
-             'hE: D0_a_to_g=7'b1001111;
-             'hF: D0_a_to_g=7'b1000111;
-             default: D0_a_to_g=7'b1111110;
-         endcase
+        if(~switchFlag)
+             case(D0_NUM)
+                 0:D0_a_to_g=7'b1111110;
+                 1:D0_a_to_g=7'b0110000;
+                 2:D0_a_to_g=7'b1101101;
+                 3:D0_a_to_g=7'b1111001;
+                 4:D0_a_to_g=7'b0110011;
+                 5:D0_a_to_g=7'b1011011;
+                 6:D0_a_to_g=7'b1011111;
+                 7:D0_a_to_g=7'b1110000;
+                 8:D0_a_to_g=7'b1111111;
+                 9:D0_a_to_g=7'b1111011;
+                 'hA: D0_a_to_g=7'b0000000; //Turn off
+                 'hB: D0_a_to_g=7'b0011111;
+                 'hC: D0_a_to_g=7'b1001110;
+                 'hD: D0_a_to_g=7'b1110110; //'N'
+                 'hE: D0_a_to_g=7'b1001111;
+                 'hF: D0_a_to_g=7'b1000111;
+                 default: D0_a_to_g=7'b1111110;
+             endcase
+         else D0_a_to_g = 7'b000_0000;
      end
 endmodule
 
@@ -66,15 +70,23 @@ module DisplayTubes( //Four bit display
      else
         ckl_cnt <= ckl_cnt + 1 ;
      end
+     
+     reg switchFlag;
  
  always@(*) //取计数变量的高 16 位组成要显示的 1 个 4 位十六进制数
      case(ckl_cnt[15:14]) //取计数变量的较低 2 位(频率高)数值，作为 4 位数码管扫描复位切换频率
-         0:begin t_led_bits = 4'b0001; dataToShow = dataBuf[3:0]; end //数码管 1 有效，且显示数值为最低 4 位
-         1:begin t_led_bits = 4'b0010; dataToShow = dataBuf[7:4]; end //数码管 2 有效，且显示数值为次低 4 位
-         2:begin t_led_bits = 4'b0100; dataToShow = dataBuf[11:8]; end //数码管 3 有效，且显示数值为次高 4 位
-         3:begin t_led_bits = 4'b1000; dataToShow = dataBuf[15:12]; end //数码管 4 有效，且显示数值为最高 4 位
+         0:begin  switchFlag = 1; t_led_bits = 4'b0001; dataToShow = dataBuf[3:0]; switchFlag = 0; end //数码管 1 有效，且显示数值为最低 4 位
+         1:begin  switchFlag = 1; t_led_bits = 4'b0010; dataToShow = dataBuf[7:4]; switchFlag = 0; end //数码管 2 有效，且显示数值为次低 4 位
+         2:begin  switchFlag = 1; t_led_bits = 4'b0100; dataToShow = dataBuf[11:8]; switchFlag = 0; end //数码管 3 有效，且显示数值为次高 4 位
+         3:begin  switchFlag = 1; t_led_bits = 4'b1000; dataToShow = dataBuf[15:12]; switchFlag = 0; end //数码管 4 有效，且显示数值为最高 4 位
      endcase
  
-D0_display myD0_display(.D0_bits(t_led_bits),.D0_NUM(dataToShow),.D0_a_to_g(a_to_g),.D0_led_bits(led_bits)) ; 
+D0_display myD0_display(
+    .D0_bits(t_led_bits),
+    .D0_NUM(dataToShow),
+    .D0_a_to_g(a_to_g),
+    .D0_led_bits(led_bits),
+    .switchFlag(switchFlag)
+) ; 
  
 endmodule
